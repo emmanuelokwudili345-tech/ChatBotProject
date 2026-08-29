fc566e1b95a5cbf6a88782aa30493b660c0cae93
Emmanuel <emmanuelokwudili345@gmail.com>
2026-08-29 02:35:10 -0700

    Install lucide from react

--- EmLuck/src/components/AppShell.tsx
+++ EmLuck/src/components/AppShell.tsx
@@ -1,9 +1,19 @@
+import { Sidebar } from "./SideBar";
+
 interface AppShellProps {
   children: React.ReactNode;
 }
 
 function AppShell({ children }: AppShellProps) {
-  return <div className="app-shell">{children}</div>;
+  return (
+    <div className="app-shell">
+      <Sidebar />
+
+      <main className="app-shell__main">
+        {children}
+      </main>
+    </div>
+  );
 }
 
 export default AppShell;
---
+++ EmLuck/src/components/SideBar.tsx
@@ -0,0 +1,34 @@
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <div className="sidebar__brand">
+        EMLUCK
+      </div>
+
+      <button className="sidebar__new-chat">
+        + New chat
+      </button>
+
+      <div className="sidebar__section">
+        <p className="sidebar__label">Recent</p>
+
+        <button className="sidebar__chat">
+          React questions
+        </button>
+
+        <button className="sidebar__chat">
+          Portfolio ideas
+        </button>
+
+        <button className="sidebar__chat">
+          JavaScript help
+        </button>
+      </div>
+
+      <div className="sidebar__bottom">
+        <button>Settings</button>
+        <button>Emmanuel</button>
+      </div>
+    </aside>
+  );
+}
